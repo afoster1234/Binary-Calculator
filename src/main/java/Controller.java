2f58eaa4c60d182d8ed1516b408b74c0f646bcfd
Austin Foster <afoster@bsu.edu>
2020-09-10 16:49:53 -0400

    Controller Functions Added

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,3 +1,4 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
@@ -9,20 +10,62 @@ public class Controller extends MainMenu {
     Pane randomPane;
     @FXML
     ToggleButton toggleBinaryDecimal;
+    @FXML
     Button zero;
+    @FXML
     Button one;
+    @FXML
     Button clear;
+    @FXML
     Button addition;
+    @FXML
     Button subtraction;
+    @FXML
     Button multiplication;
+    @FXML
     Button division;
+    @FXML
     Button squared;
+    @FXML
     Button squareRoot;
+    @FXML
     Button equals;
 
     public void initialize(){
 
     }
 
+    public void toggleBinaryDecimal(ActionEvent actionEvent) {
+    }
+
+    public void zero(ActionEvent actionEvent) {
+    }
+
+    public void clear(ActionEvent actionEvent) {
+    }
+
+    public void one(ActionEvent actionEvent) {
+    }
+
+    public void addition(ActionEvent actionEvent) {
+    }
+
+    public void subtraction(ActionEvent actionEvent) {
+    }
+
+    public void squareRoot(ActionEvent actionEvent) {
+    }
+
+    public void squared(ActionEvent actionEvent) {
+    }
+
+    public void multiplication(ActionEvent actionEvent) {
+    }
+
+    public void division(ActionEvent actionEvent) {
+    }
+
+    public void equals(ActionEvent actionEvent) {
+    }
 }
 
