0357c6be724cb59ed0513c5bdb1d7b4caa93bc2a
Austin Foster <afoster@bsu.edu>
2020-09-10 16:47:17 -0400

    Controller Buttons Added

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,13 +1,24 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 
 public class Controller extends MainMenu {
 
     @FXML
     Pane randomPane;
-
+    @FXML
+    ToggleButton toggleBinaryDecimal;
+    Button zero;
+    Button one;
+    Button clear;
+    Button addition;
+    Button subtraction;
+    Button multiplication;
+    Button division;
+    Button squared;
+    Button squareRoot;
+    Button equals;
 
     public void initialize(){
 
