3d610ffef2ae721d3fcce2536db2c603b8df9f2f
Matthew <maschrag@bsu.edu>
2020-09-10 17:34:16 -0400

    Added square and squareroot functions

--- src/main/java/ArithmeticOperators.java
+++ src/main/java/ArithmeticOperators.java
@@ -23,4 +23,15 @@ public class ArithmeticOperators {
         int int2 = Convertor.toDecimal(binary2);
         return Convertor.toBinary(int1 / int2);
     }
+
+    public String square(String binary1) {
+        int int1 = Convertor.toDecimal(binary1);
+        return Convertor.toBinary(int1 ^ int1);
+    }
+
+    public String squareroot(String binary1) {
+        int int1 = Convertor.toDecimal(binary1);
+        Math.sqrt(int1);
+        return Convertor.toBinary(int1);
+    }
 }
