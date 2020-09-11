24c8b0963320de438dc828731fa2541f6bcd78b8
Austin Foster <afoster@bsu.edu>
2020-09-11 13:56:22 -0400

    Controller Class Modified

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,16 +1,16 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
 
 public class Controller extends MainMenu {
     //calculator
-    @FXML Pane randomPane;
+    @FXML TextArea calculator;
     @FXML Button decimalToggle;
     @FXML Button binaryToggle;
     @FXML Button clear;
     @FXML Button equals;
-    @FXML Button backspace;
     //operators
     @FXML Button zero;
     @FXML Button one;
@@ -23,42 +23,51 @@ public class Controller extends MainMenu {
 
 
     public void zero(ActionEvent actionEvent) {
+
     }
 
     public void clear(ActionEvent actionEvent) {
+
     }
 
     public void one(ActionEvent actionEvent) {
+
     }
 
     public void addition(ActionEvent actionEvent) {
+
     }
 
     public void subtraction(ActionEvent actionEvent) {
+
     }
 
     public void squareRoot(ActionEvent actionEvent) {
+
     }
 
     public void squared(ActionEvent actionEvent) {
+
     }
 
     public void multiplication(ActionEvent actionEvent) {
+
     }
 
     public void division(ActionEvent actionEvent) {
+
     }
 
     public void equals(ActionEvent actionEvent) {
+
     }
 
     public void binaryToggle(ActionEvent actionEvent) {
+
     }
 
     public void decimalToggle(ActionEvent actionEvent) {
-    }
 
-    public void backspace(ActionEvent actionEvent) {
     }
 }
 
