d1677789dd0e372744442b21a4462dc71bb15735
Robert Klueber <rcklueber@bsu.edu>
2020-09-11 14:54:12 -0400

    Made working calculator, bridging gap between logic and UI.

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,72 +1,126 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
 
 public class Controller extends MainMenu {
     //calculator
-    @FXML TextArea calculator;
-    @FXML Button decimalToggle;
-    @FXML Button binaryToggle;
-    @FXML Button clear;
-    @FXML Button equals;
+    @FXML
+    Label calculator;
+    @FXML
+    Button Toggle;
+    @FXML
+    Button clear;
+    @FXML
+    Button equals;
     //operators
-    @FXML Button zero;
-    @FXML Button one;
-    @FXML Button addition;
-    @FXML Button subtraction;
-    @FXML Button multiplication;
-    @FXML Button division;
-    @FXML Button squared;
-    @FXML Button squareRoot;
+    @FXML
+    Button zero;
+    @FXML
+    Button one;
+    @FXML
+    Button addition;
+    @FXML
+    Button subtraction;
+    @FXML
+    Button multiplication;
+    @FXML
+    Button division;
+    @FXML
+    Button squared;
+    @FXML
+    Button squareRoot;
 
+    ArithmeticOperators arithmetic = new ArithmeticOperators();
+
+    String numberFormat = "binary";
+    String value1;
+    String value2;
+    String operator = "";
 
     public void zero(ActionEvent actionEvent) {
-
+        calculator.setText(calculator.getText() + "0");
     }
 
     public void clear(ActionEvent actionEvent) {
-
+        calculator.setText("");
+        value1 = "";
+        value2 = "";
+        numberFormat = "binary";
+        operator = "";
     }
 
     public void one(ActionEvent actionEvent) {
-
+        calculator.setText(calculator.getText() + "1");
     }
 
     public void addition(ActionEvent actionEvent) {
-
+        value1 = calculator.getText();
+        operator = "addition";
+        calculator.setText("");
     }
 
     public void subtraction(ActionEvent actionEvent) {
-
+        value1 = calculator.getText();
+        operator = "subtraction";
+        calculator.setText("");
     }
 
     public void squareRoot(ActionEvent actionEvent) {
-
+        arithmetic.squareroot(calculator.getText());
     }
 
     public void squared(ActionEvent actionEvent) {
-
+        arithmetic.square(calculator.getText());
     }
 
     public void multiplication(ActionEvent actionEvent) {
-
+        value1 = calculator.getText();
+        operator = "multiplication";
+        calculator.setText("");
     }
 
     public void division(ActionEvent actionEvent) {
+        value1 = calculator.getText();
+        operator = "division";
+        calculator.setText("");
+    }
+
+    public void equals(ActionEvent actionEvent) throws Exception {
+        if (value1.equals("")) {
+            return;
+        }
+        value2 = calculator.getText();
+        switch (operator) {
+            case "addition":
+                calculator.setText(arithmetic.addition(value1, value2));
+                break;
+            case "subtraction":
+                calculator.setText(arithmetic.subtraction(value1, value2));
+                break;
+            case "multiplication":
+                calculator.setText(arithmetic.multiplication(value1, value2));
+                break;
+            case "division":
+                calculator.setText(arithmetic.division(value1, value2));
+                break;
+            default:
+                throw new Exception();
+        }
 
     }
 
-    public void equals(ActionEvent actionEvent) {
-
-    }
-
-    public void binaryToggle(ActionEvent actionEvent) {
-
-    }
-
-    public void decimalToggle(ActionEvent actionEvent) {
+    public void Toggle(ActionEvent actionEvent) {
+        if (numberFormat.equals("decimal")) {
+            Convertor.toBinary(Integer.parseInt(calculator.getText()));
+            numberFormat = "binary";
+        }
+        else if(numberFormat.equals("binary")){
+            Convertor.toDecimal(calculator.getText());
+            numberFormat = "decimal";
+        }
 
     }
 }
