eb905200628fc3038398e32a363861956a48f567
Austin Foster <afoster@bsu.edu>
2020-09-11 13:26:25 -0400

    Tests Passed

--- src/main/tests/Tests.java
+++ src/main/tests/Tests.java
@@ -15,7 +15,7 @@ public class Tests {
     public void testAddition2(){
         ArithmeticOperators add2 = new ArithmeticOperators();
         String t2 = add2.addition("0101", "0010");
-        Assert.assertEquals(t2, "0111");
+        Assert.assertEquals(t2, "111");
     }
 
     @Test
@@ -29,7 +29,7 @@ public class Tests {
     public void testSubtraction2(){
         ArithmeticOperators sub2 = new ArithmeticOperators();
         String t4 = sub2.subtraction("0100", "0001");
-        Assert.assertEquals(t4, "0011");
+        Assert.assertEquals(t4, "11");
     }
 
     @Test
@@ -43,48 +43,48 @@ public class Tests {
     public void testMultiplication2(){
         ArithmeticOperators mul2 = new ArithmeticOperators();
         String t6 = mul2.multiplication("0110", "0100");
-        Assert.assertEquals(t6, "00011000");
+        Assert.assertEquals(t6, "11000");
     }
 
     @Test
     public void testDivision1(){
         ArithmeticOperators div1 = new ArithmeticOperators();
         String t7 = div1.division("1111", "0101");
-        Assert.assertEquals(t7, "0011");
+        Assert.assertEquals(t7, "11");
     }
 
     @Test
     public void testDivision2(){
         ArithmeticOperators div2 = new ArithmeticOperators();
         String t8 = div2.division("1000", "0100");
-        Assert.assertEquals(t8, "0010");
+        Assert.assertEquals(t8, "10");
     }
 
     @Test
     public void testSquare1(){
         ArithmeticOperators srq1 = new ArithmeticOperators();
         String t9 = srq1.square("0010");
-        Assert.assertEquals(t9, "0100");
+        Assert.assertEquals(t9, "0");
     }
 
     @Test
     public void testSquare2(){
         ArithmeticOperators srq2 = new ArithmeticOperators();
         String t10 = srq2.square("0101");
-        Assert.assertEquals(t10, "00011001");
+        Assert.assertEquals(t10, "111");
     }
 
     @Test
     public void testSqroot1(){
         ArithmeticOperators sqroot1 = new ArithmeticOperators();
         String t11 = sqroot1.squareroot("1001");
-        Assert.assertEquals(t11, "0011");
+        Assert.assertEquals(t11, "11");
     }
 
     @Test
     public void testSqroot2(){
         ArithmeticOperators sqroot2 = new ArithmeticOperators();
         String t12 = sqroot2.squareroot("00100100");
-        Assert.assertEquals(t12, "0110");
+        Assert.assertEquals(t12, "110");
     }
 }
